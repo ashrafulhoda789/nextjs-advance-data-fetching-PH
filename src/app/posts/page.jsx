8775a82a6cfc80519fe76ea6b72db09410e8ea91
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-22 01:17:18 +0600

    different way of data fetching

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,9 +1,44 @@
 
+// way-2
+// const getPosts = async() =>{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//     return res.json();
+// }
 
-const PostsPage = () => {
+
+// way-3
+// const getPosts = async () => {
+//     try {
+//         const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//         return res.json();
+//     }
+//     catch(err){
+//         throw new Error('Failed to fetch posts');
+//     }
+// }
+
+// way-4
+const getPosts = async() =>{
+    
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    if(!res.ok){
+        throw new Error('Failed to fetch data');
+    }
+    return res.json();
+}
+
+
+const PostsPage = async () => {
+    // way-1
+    // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    // const posts = await res.json();
+
+    const posts = await getPosts();
+
     return (
         <div>
-            <h2>Posts</h2>
+            <h2>Posts: {posts.length}</h2>
         </div>
     );
 };
