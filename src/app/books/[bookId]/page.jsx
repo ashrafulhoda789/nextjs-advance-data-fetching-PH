a9a4b32f94dff2b803a3ab9c55889f94b48506c7
ashraful-hoda <asrafulhoda789@gmail.com>
2026-04-23 23:01:03 +0600

    understand generateStaticParams

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -1,10 +1,10 @@
 
-// export const generateStaticParams = async() =>{
-//     const res = await fetch('http://localhost:5000/books');
-//     const books = await res.json();
+export const generateStaticParams = async() =>{
+    const res = await fetch('http://localhost:5000/books');
+    const books = await res.json();
     
-//     return books.slice(1,3).map(book => ({bookId: book.id}));
-// }
+    return books.slice(1,3).map(book => ({bookId: book.id}));
+}
 
 
 const BookDetailPage = async ({ params }) => {
